3607fb0fd48278a871a74e38fca1379dc2c9d31d
dtgarzon <dtgarzon@espol.edu.ec>
2018-10-21 13:41:10 -0500

    dashboard Funcional

--- js/script.js
+++ js/script.js
@@ -16,6 +16,7 @@ $(document).ready(function(){
 		valor = $("#tipo").val();
 		factor = $("#factor").val();
 		anio = $("#anno").val();
+		
 		//factor = //
 		// algo mas....
 		console.log(valor);
@@ -23,6 +24,7 @@ $(document).ready(function(){
 		console.log(factor);
 		// ../
 		nombreArchivo = "";
+		
 		if (factor =="precipitation"){
 			nombreArchivo = "precipitation/" + factor + "_" + anio + "_" + valor + ".html"; 
 			$("#charExample").load(nombreArchivo);
@@ -52,6 +54,29 @@ $(document).ready(function(){
 		// $("#charExample").attr("src", "./img/plot.jpeg");
 
 	})
+
+	
+	$("#enfermedad").change(function(){
+		factor = $("#factor").val();
+		anio = $("#anno").val();
+		enf = $("#enfermedad").val();
+		nombreArchivo2= "";
+		if(enf =="malaria"){
+			nombreArchivo2 = "correlaciones/malaria/" + factor + "_" + anio + ".html";	
+			$("#charExample2").load(nombreArchivo2);
+		}
+		if(enf =="rabia"){
+			nombreArchivo2 = "correlaciones/rabia/" + factor + "_" + anio + ".html";	
+			$("#charExample2").load(nombreArchivo2);
+		}
+		if(enf =="tuberculosis"){
+			nombreArchivo2 = "correlaciones/tuberculosis/" + factor + "_" + anio + ".html";	
+			$("#charExample2").load(nombreArchivo2);
+		}
+
+	});
+	
+
 
 
 
